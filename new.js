f3afe875b672b9a19ab0555648ecb4949eb94693
Divy Gupta <divygupta208@gmail.com>
2024-07-12 02:27:48 +0530

    route

--- new.js
+++ new.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/home") {
+    res.write("<html>");
+    res.write("<body><h1>Home Page</h1></body>");
+    res.write("</html>");
+  }
+  if (url === "/about") {
+    res.write("<html>");
+    res.write("<body><h1>About Page</h1></body>");
+    res.write("</html>");
+  }
+  if (url === "/node") {
+    res.write("<html>");
+    res.write("<body><h1>Welcone to node Page</h1></body>");
+    res.write("</html>");
+  }
+});
+
+server.listen(3000);
