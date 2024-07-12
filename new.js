b7ec5da5070bb5b6858738f117a6fcd60a84c3d9
Divy Gupta <divygupta208@gmail.com>
2024-07-13 03:57:21 +0530

    demo 1

--- new.js
+++ new.js
@@ -1,23 +1,47 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
-  if (url === "/home") {
-    res.write("<html>");
-    res.write("<body><h1>Home Page</h1></body>");
-    res.write("</html>");
-  }
-  if (url === "/about") {
-    res.write("<html>");
-    res.write("<body><h1>About Page</h1></body>");
-    res.write("</html>");
-  }
-  if (url === "/node") {
-    res.write("<html>");
-    res.write("<body><h1>Welcone to node Page</h1></body>");
-    res.write("</html>");
+  if (url === "/") {
+    if (method === "GET") {
+      res.write("<html>");
+      res.write(
+        "<body><form method='POST' action='/'><input name='message' type='text'><button type='submit'>Send</button></form></body>"
+      );
+      res.write("</html>");
+      return res.end();
+    }
+
+    if (method === "POST") {
+      const body = [];
+
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1].replace(/\+/g, " ");
+
+        fs.writeFileSync("message.txt", message);
+
+        res.write("<html>");
+        res.write("<body>");
+        res.write(`<p>Message received: ${message}</p>`);
+        res.write(
+          "<form method='POST' action='/'><input name='message' type='text'><button type='submit'>Send</button></form>"
+        );
+        res.write("</body>");
+        res.write("</html>");
+        return res.end();
+      });
+    }
   }
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
