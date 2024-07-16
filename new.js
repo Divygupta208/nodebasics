8c2729efaab5c1075c8617ff6c428ec5e675fb8e
Divy Gupta <divygupta208@gmail.com>
2024-07-17 00:59:11 +0530

    express

--- new.js
+++ new.js
@@ -1,47 +1,20 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  if (url === "/") {
-    if (method === "GET") {
-      res.write("<html>");
-      res.write(
-        "<body><form method='POST' action='/'><input name='message' type='text'><button type='submit'>Send</button></form></body>"
-      );
-      res.write("</html>");
-      return res.end();
-    }
-
-    if (method === "POST") {
-      const body = [];
-
-      req.on("data", (chunk) => {
-        body.push(chunk);
-      });
-
-      req.on("end", () => {
-        const parsedBody = Buffer.concat(body).toString();
-        const message = parsedBody.split("=")[1].replace(/\+/g, " ");
-
-        fs.writeFileSync("message.txt", message);
-
-        res.write("<html>");
-        res.write("<body>");
-        res.write(`<p>Message received: ${message}</p>`);
-        res.write(
-          "<form method='POST' action='/'><input name='message' type='text'><button type='submit'>Send</button></form>"
-        );
-        res.write("</body>");
-        res.write("</html>");
-        return res.end();
-      });
-    }
-  }
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<html><body><form action="/product" method="POST" ><input name="title" type="text"><button type="submit">Add product</button></form></body></html>'
+  );
 });
 
-server.listen(3000, () => {
+app.use("/product", (req, res, next) => {
+  const body = req.body;
+  console.log(body);
+});
+
+app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
