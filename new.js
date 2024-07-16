7a351c0f73a630796724a0dd01644ce4120d24ae
Divy Gupta <divygupta208@gmail.com>
2024-07-17 01:24:44 +0530

    middlewares

--- new.js
+++ new.js
@@ -4,15 +4,22 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/add-product", (req, res, next) => {
+app.get("/add-product", (req, res, next) => {
   res.send(
-    '<html><body><form action="/product" method="POST" ><input name="title" type="text"><button type="submit">Add product</button></form></body></html>'
+    '<html><body><form action="/product" method="POST" ><input name="title" type="text"><input type="number" name="size" ><button type="submit">Add product</button></form></body></html>'
   );
 });
 
-app.use("/product", (req, res, next) => {
-  const body = req.body;
+let body = {};
+
+app.post("/product", (req, res, next) => {
+  body = req.body;
+  res.redirect("/");
   console.log(body);
+});
+
+app.use("/", (req, res, next) => {
+  res.send(`<h1>${body.title}</h1>`);
 });
 
 app.listen(3000, () => {
